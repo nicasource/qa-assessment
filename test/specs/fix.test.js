2fa97d2a39e896446600171802d1cd84ffef7bc2
sloaisiga <sloaisiga18@gmail.com>
2025-09-18 17:49:00 -0600

    add test cases for assessment

--- test/specs/fix.test.js
+++ test/specs/fix.test.js
@@ -1,6 +1,15 @@
+import ContactFormPage from "../pageobjects/contact.page.js"
 //This test will focus on fixing an existing test
-describe('Fixture', () => {
-    it('should send a general inquiries message', () => {
-        
-    });
+describe('Fix Contact form: New Project', () => {
+     it('should submit contact form successfully and redirect to thank you page', async () => {
+            const contact = await ContactFormPage.generateContactModel()
+    
+            await ContactFormPage.open()
+            await ContactFormPage.selectContactMenu()
+            await ContactFormPage.fillForm(contact)
+    
+            await browser.pause(3000)
+            const currentUrl = await browser.getUrl()
+            expect(currentUrl).toEqual(browser.options.baseUrl + "thank-you")
+        })
 })
