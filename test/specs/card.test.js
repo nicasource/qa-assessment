1fe51987f13e5144eeb76a3abb214cdff0b2b6f5
JustinLorente <lorentelopezjustin1975@gmail.com>
2025-09-25 14:43:35 -0600

    add: card test

--- test/specs/card.test.js
+++ test/specs/card.test.js
@@ -12,8 +12,33 @@ describe('Credit Card', () => {
         const cardElement = $('.braintree-option.braintree-option__card')
         await cardElement.waitForClickable()
         await cardElement.click()
-        
+
         // TODO: Fill the payment form with the credit card information
+
+        const numberFrame = await $('iframe[name="braintree-hosted-field-number"]')
+        await browser.switchToFrame(numberFrame)
+        await $('#credit-card-number').waitForDisplayed({ timeout: 10000 })
+        await $('#credit-card-number').setValue(creditCard.number)
+        await browser.switchToParentFrame()
+
+        const expirationFrame = await $('iframe[name="braintree-hosted-field-expirationDate"]')
+        await browser.switchToFrame(expirationFrame)
+        await $('#expiration').waitForDisplayed({ timeout: 10000 })
+        await $('#expiration').setValue(creditCard.expDate)
+        await browser.switchToParentFrame()
+
+        const cvvFrame = await $('iframe[name="braintree-hosted-field-cvv"]')
+        await browser.switchToFrame(cvvFrame)
+        await $('#cvv').waitForDisplayed({ timeout: 10000 })
+        await $('#cvv').setValue(creditCard.cvv)
+        await browser.switchToParentFrame()
+
+        const postalFrame = await $('iframe[name="braintree-hosted-field-postalCode"]')
+        await browser.switchToFrame(postalFrame)
+        await $('#postal-code').waitForDisplayed({ timeout: 10000 })
+        await $('#postal-code').setValue(creditCard.postalCode)
+        await browser.switchToParentFrame()
+
 
         const payBtn = $('#pay-button')
         await payBtn.waitForClickable()
